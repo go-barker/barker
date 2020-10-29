ebbdc33af5ff3b2ea917a122af8fddff458ce355
no <no>
2020-10-29 11:31:47 +0200

    add semicolon

--- ts/src/dao.ts
+++ ts/src/dao.ts
@@ -15,7 +15,7 @@ export interface BotDao {
     Update(bot: Bot): Promise<Bot>;
     Get(botID: number): Promise<Bot>;
     List(pageRequest: PaginatorRequest): Promise<[Bot[], PaginatorResponse]>;
-    RRTake(): Promise<Bot>
+    RRTake(): Promise<Bot>;
 }
 
 export interface CampaignDao {
